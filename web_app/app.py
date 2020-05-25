ac0b7e7da4598e891e51d50c57651a38be6c8d7a
Annie Rumbles <amrumbles@gmail.com>
2020-05-25 12:02:54 -0600

    flask app mods

--- web_app/app.py
+++ web_app/app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
+import pickle
+import pandas as pd
+import numpy as np
 app = Flask(__name__)
+
+with open('../data/df.pkl', 'rb') as f:
+    df = pickle.load(f)
+
+hike_features= []
 
 # home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/trails')
+@app.route('/next_trail')
 def get_recommendation():
+    pass
+
+@app.route('/get_started')
+def group_topics():
     pass
 
 if __name__ == '__main__':
