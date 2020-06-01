f0f084bebdc04b8e8a6a41a52c3de8d37c238258
Annie Rumbles <amrumbles@gmail.com>
2020-06-01 16:25:32 -0600

    created get started page and pages for topics

--- web_app/app.py
+++ web_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-import pickle
+import pickle, random
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
@@ -53,6 +53,9 @@ df_merged = import_csv('../data/topics_and_numericalfeatures.csv')
 df_merged.drop(['difficulty_hard', 'difficulty_moderate', 'out_and_back', 'point_to_point'], axis=1, inplace=True)
 df_dogs_allowed = import_csv('../data/dogs_allowed.csv')
 hike_urls = hike_url_dict(df_raw)
+df_topics = df_merged[['topic_1', 'topic_2', 'topic_3', 'topic_4', 'topic_5', 'topic_6', 'topic_7', 'topic_8']]
+majority = [np.argmax(df_topics.loc[i])+1 for i in df_topics.index]
+df_topics['majority_topics'] = majority
 
 hike_features= ['Hike Name', 'Difficulty', 'Hike Type', 'Location', 'Distance (miles)', 'Elevation Gain (feet)', 'Dogs Allowed?']
 df_scaled, similarity_df, X = make_sim_matrix(df_merged)
@@ -79,11 +82,91 @@ def get_hike_form():
 
 @app.route('/get_started')
 def group_topics():
-    pass
+    return render_template('get_started.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/bang-for-your-buck')
+def t1():
+    title = 'Bang for your Buck'
+    desc = 'These hikes are solid options if you want an all around trek!'
+    recs_all = df_topics[df_topics['majority_topics']==1].index.tolist()
+    recs_samp = random.sample(recs_all, 20)
+    specs = get_hike_specs(recs_samp)
+    return render_template('topics.html', hikes=recs_samp, hike_features=hike_features, hike_specs=specs, title=title, 
+        description=desc, urls=hike_urls, dogs=df_dogs_allowed.index)
+
+@app.route('/flora-and-fauna')
+def t2():
+    title = 'Flora and Fauna'
+    desc = 'For nature lovers!'
+    recs_all = df_topics[df_topics['majority_topics']==2].index.tolist()
+    recs_samp = random.sample(recs_all, 20)
+    specs = get_hike_specs(recs_samp)
+    return render_template('topics.html', hikes=recs_samp, hike_features=hike_features, hike_specs=specs, title=title, 
+        description=desc, urls=hike_urls, dogs=df_dogs_allowed.index)
+
+@app.route('/backcountry-activites')
+def t3():
+    title = 'Backcountry Activities'
+    desc = 'Whether you want to fish, backpack, or camp in the backcountry'
+    recs_all = df_topics[df_topics['majority_topics']==3].index.tolist()
+    recs_samp = random.sample(recs_all, 20)
+    specs = get_hike_specs(recs_samp)
+    return render_template('topics.html', hikes=recs_samp, hike_features=hike_features, hike_specs=specs, title=title, 
+        description=desc, urls=hike_urls, dogs=df_dogs_allowed.index)
+
+@app.route('/offroad')
+def t4():
+    title = 'Off-Roading'
+    desc = 'Options for people who want to explore via 4x4'
+    recs_all = df_topics[df_topics['majority_topics']==4].index.tolist()
+    recs_samp = random.sample(recs_all, 20)
+    specs = get_hike_specs(recs_samp)
+    return render_template('topics.html', hikes=recs_samp, hike_features=hike_features, hike_specs=specs, title=title, 
+        description=desc, urls=hike_urls, dogs=df_dogs_allowed.index)
+
+@app.route('/adventures-with-pup')
+def t5():
+    title = 'Adventures with the Pup'
+    desc = 'Bonding time with your best friend'
+    recs_all = df_topics[df_topics['majority_topics']==5].index.tolist()
+    recs_samp = random.sample(recs_all, 20)
+    specs = get_hike_specs(recs_samp)
+    return render_template('topics.html', hikes=recs_samp, hike_features=hike_features, hike_specs=specs, title=title, 
+        description=desc, urls=hike_urls, dogs=df_dogs_allowed.index)
+
+@app.route('/water-seekers')
+def t6():
+    title = 'Water Feature-Seekers'
+    desc = 'For waterfall and river admirers'
+    recs_all = df_topics[df_topics['majority_topics']==6].index.tolist()
+    recs_samp = random.sample(recs_all, 20)
+    specs = get_hike_specs(recs_samp)
+    return render_template('topics.html', hikes=recs_samp, hike_features=hike_features, hike_specs=specs, title=title, 
+        description=desc, urls=hike_urls, dogs=df_dogs_allowed.index)
+
+@app.route('/mtb')
+def t7():
+    title = 'MTB'
+    desc = 'Mountain bikers - these are for you!'
+    recs_all = df_topics[df_topics['majority_topics']==7].index.tolist()
+    recs_samp = random.sample(recs_all, 20)
+    specs = get_hike_specs(recs_samp)
+    return render_template('topics.html', hikes=recs_samp, hike_features=hike_features, hike_specs=specs, title=title, 
+        description=desc, urls=hike_urls, dogs=df_dogs_allowed.index)
+
+@app.route('/peak-baggers')
+def t8():
+    title = 'Peak-Baggers'
+    desc = 'Reach new heights!'
+    recs_all = df_topics[df_topics['majority_topics']==8].index.tolist()
+    recs_samp = random.sample(recs_all, 20)
+    specs = get_hike_specs(recs_samp)
+    return render_template('topics.html', hikes=recs_samp, hike_features=hike_features, hike_specs=specs, title=title, 
+        description=desc, urls=hike_urls, dogs=df_dogs_allowed.index)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, threaded=True, debug=False)
