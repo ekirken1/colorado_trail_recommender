5797bb5c8446945d59d5d331f2c033f90e3bf82a
ekirken1 <kirkendall.eve@gmail.com>
2020-06-13 19:49:11 -0600

    allow chromedriver to save and read html in txt file; update requirements for lxml.

--- src/chromedriver_class.py
+++ src/chromedriver_class.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 class ChromeDriver(object):
     def __init__(self):
-        self.driver = webdriver.Chrome('/Users/annierumbles/Downloads/chromedriver')
+        self.driver = webdriver.Chrome()
         self.all_hike_urls = None
 
     def _load_urls(self, url_of_pageofurls):
@@ -16,7 +16,9 @@ class ChromeDriver(object):
         time.sleep(4)
         while True:
             try:
-                loadMoreButton = self.driver.find_element_by_xpath("//button[contains(text(), 'Show more trails')]")
+                loadMoreButton = self.driver.find_element_by_xpath(
+                    "//button[contains(text(), 'Show more trails')]"
+                    )
                 time.sleep(2)
                 loadMoreButton.click()
                 time.sleep(8)
@@ -25,20 +27,34 @@ class ChromeDriver(object):
                 html = self.driver.page_source
                 return html
 
-    def get_urls(self, url):
-        html = self._load_urls(url)
+    
+    def save_urls_to_txt(self, url, filename):
+        ''' Fetch and save html from website.
+        '''
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                html = f.read()
+        else:
+            html = self._load_urls(url)
+            with open(filename, 'w') as f:
+                f.write(html)
+        return html
+
+
+    def get_urls(self, url, filename):
+        ''' Parse html for the trails' urls and save urls.
+        '''
+        html = self.save_urls_to_txt(url, filename)
         soup = self._make_soup(html)
         links = soup.find_all('a', itemprop='url')
         self.all_hike_urls = ['https://www.alltrails.com'+links[i]['href'] for i in range(len(links))]
         return self.all_hike_urls
 
-    def save_urls_to_json(self, filename):
-        if self.all_hike_urls == None:
-            print("No urls found")
-            return
-        else:
-            with open(filename, 'w', encoding='utf-8') as f:
-                json.dump(self.all_hike_urls, f, ensure_ascii=False, indent=4)
+    def save_urls_to_json(self, url, filename_html, filename_json):
+        self.all_hike_urls = self.get_urls(url, filename_html)
+        with open(filename_json, 'w', encoding='utf-8') as f:
+            json.dump(self.all_hike_urls, f, ensure_ascii=False, indent=4)
+            
 
     def load_reviews(self, url):
         while True:
@@ -54,6 +70,3 @@ class ChromeDriver(object):
         soup = BeautifulSoup(html, 'lxml')
         return soup
 
-
-
-    
